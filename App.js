6e24f012ad7454d7839b63b8637ce8a328e1d26a
Mikael Rizki <mikael.rizki.pe@gmail.com>
2024-09-08 12:19:07 +0700

    Update: Setup Font

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { View } from "react-native";
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import Router from "./src/services/router";
+import FONTS from "./src/assets/fonts";
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts(FONTS.KhumbhSans);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <NavigationContainer>
---
+++ src/assets/fonts/index.js
@@ -0,0 +1,12 @@
+const FONTS = {
+  KhumbhSans: {
+    'extraBold': require('./KumbhSans-ExtraBold.ttf'),
+    'bold': require('./KumbhSans-Bold.ttf'),
+    'semiBold': require('./KumbhSans-SemiBold.ttf'),
+    'regular': require('./KumbhSans-Regular.ttf'),
+    'medium': require('./KumbhSans-Medium.ttf'),
+    'light': require('./KumbhSans-Light.ttf'),
+  },
+};
+
+export default FONTS;
