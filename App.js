34dc3b7581dd4884bba200ea4dcdc1c5cdb84237
Mikael Rizki <mikael.rizki.pe@gmail.com>
2024-09-15 10:29:44 +0700

    Feat: Transparent NavBar Android

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import Router from "./src/services/router";
 import FONTS from "./src/assets/fonts";
+import * as NavigationBar from "expo-navigation-bar";
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts(FONTS.KhumbhSans);
@@ -11,6 +12,8 @@ export default function App() {
   if (!fontsLoaded && !fontError) {
     return null;
   }
+
+  NavigationBar.setVisibilityAsync("hidden");
 
   return (
     <View style={{ flex: 1 }}>
