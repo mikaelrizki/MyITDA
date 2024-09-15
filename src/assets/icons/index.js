73e2e5a319efb6a66563743e059b22443c34b51c
Mikael Rizki <mikael.rizki.pe@gmail.com>
2024-09-15 17:19:11 +0700

    Update: Setup Icon and Image Asset

---
+++ src/assets/icons/index.js
@@ -0,0 +1,6 @@
+const ICONS = {
+  iconNIM: require("./iconNIM.png"),
+  iconKey: require("./iconKey.png"),
+};
+
+export default ICONS;
---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+const IMAGES = {
+    bgSplash: require('./bgSplash.png'),
+    bgDefault: require('./bgDefault.png'),
+    logoITDA: require('./logoITDA.png'),
+};
+
+export default IMAGES;
