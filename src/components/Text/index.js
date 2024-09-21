dbda4c065f6c34f58726d0b674798a62599b8f0a
Mikael Rizki <mikael.rizki.pe@gmail.com>
2024-09-21 09:45:56 +0700

    Feat: Splash Screen

---
+++ src/components/Text/index.js
@@ -0,0 +1,43 @@
+import { Text as RNText } from "react-native";
+import { COLORS, SIZES } from "../../styles";
+
+export default function Text({
+  fontsize = SIZES.font,
+  padVertical = SIZES.padding,
+  color = COLORS.primary,
+  children,
+  extraBold = false,
+  bold = false,
+  semiBold = false,
+  medium = false,
+  regular = false,
+  light = false,
+  center = false,
+  opacity = 1,
+  style,
+  ...props
+}) {
+  return (
+    <RNText
+      {...props}
+      style={[
+        {
+          fontSize: fontsize,
+          color: color,
+          paddingVertical: padVertical,
+          textAlign: center ? "center" : "left",
+          opacity,
+        },
+        extraBold && { fontFamily: "extraBold" },
+        bold && { fontFamily: "bold" },
+        semiBold && { fontFamily: "semiBold" },
+        medium && { fontFamily: "medium" },
+        regular && { fontFamily: "regular" },
+        light && { fontFamily: "light" },
+        style,
+      ]}
+    >
+      {children}
+    </RNText>
+  );
+}
--- src/pages/splash/index.js
+++ src/pages/splash/index.js
@@ -1,24 +1,42 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { COLORS, SIZES } from "../../styles";
+import React, { useEffect } from 'react';
+import { Image, ImageBackground } from "react-native";
+import IMAGES from "../../assets/images";
+import Text from "../../components/Text";
+import { SIZES } from "../../styles";
 
-export default function SplashScreen() {
+export default function SplashScreen({ navigation }) {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Auth");
+    }, 2000);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MyITDA</Text>
-    </View>
+    <ImageBackground
+      source={IMAGES.bgSplash}
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        source={IMAGES.logoITDA}
+        style={{ height: 140, marginBottom: 10 }}
+        resizeMode="contain"
+      />
+      <Text center bold fontsize={SIZES.LargeText}>
+        MyITDA
+      </Text>
+      <Text
+        center
+        regular
+        fontsize={SIZES.smallText}
+        padVertical={0}
+        style={{ marginBottom: 120 }}
+      >
+        Student Portal Apps of{"\n"}Institut Teknologi Dirgantara Adisutjipto
+      </Text>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.primary,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: SIZES.largeText,
-    color: COLORS.white,
-  },
-});
