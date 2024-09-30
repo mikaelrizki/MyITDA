c3d6680705d907a54bbeb47bfd62cbfd3c81334b
Michael Fidef Natalino <93313950+michaelfidef@users.noreply.github.com>
2024-09-30 20:10:59 +0700

    Feat: Slicing Transkrip Screen

---
+++ src/components/TableTranskrip/index.js
@@ -0,0 +1,188 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { COLORS, SIZES } from "../../styles";
+import Text from "../Text";
+import SksBadge from "../SksBadge";
+
+export default function TableTranskrip({ judul }) {
+    let displayedJudul;
+
+  if (judul == "wajib") {
+    displayedJudul = "Total Sks WAJIB";
+  } else if(judul == "pilihan") {
+    displayedJudul = "Total Sks PILIHAN";
+  }else {
+    displayedJudul = "tidak ada judul"
+  }
+  return (
+    <>
+      <View style={LOKAL_STYLES.tableRow}>
+        <Text
+          bold
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "12%" }}
+        >
+          KODE
+        </Text>
+        <Text
+          bold
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ flex: 1 }}
+        >
+          MATA KULIAH
+        </Text>
+        <Text
+          bold
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "10%" }}
+        >
+          SKS
+        </Text>
+        <Text
+          bold
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "10%" }}
+        >
+          NILAI
+        </Text>
+        <Text
+          bold
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "20%" }}
+        >
+          KUALITAS
+        </Text>
+      </View>
+
+      <View style={LOKAL_STYLES.itemTableRow}>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "12%" }}
+        >
+          IF001
+        </Text>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ flex: 1 }}
+        >
+          LOGIKA MATEMATIKA
+        </Text>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "10%" }}
+        >
+          3
+        </Text>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "10%" }}
+        >
+          A
+        </Text>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "20%" }}
+        >
+          12
+        </Text>
+      </View>
+      <View style={LOKAL_STYLES.itemTableRow}>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "12%" }}
+        >
+          IF002
+        </Text>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ flex: 1 }}
+        >
+          SISTEM PAKAR
+        </Text>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "10%" }}
+        >
+          3
+        </Text>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "10%" }}
+        >
+          B
+        </Text>
+        <Text
+          color={COLORS.black}
+          fontsize={SIZES.smallText}
+          style={{ width: "20%" }}
+        >
+          9
+        </Text>
+      </View>
+
+      {/* TOTAL SELURUHNYA */}
+      <View style={LOKAL_STYLES.itemTableRow}>
+        <View style={LOKAL_STYLES.sksContainer}>
+          <View style={LOKAL_STYLES.sksBadge}>
+            <Text color={COLORS.white} fontsize={SIZES.smallText}>
+              {displayedJudul}
+            </Text>
+          </View>
+        </View>
+        <View style={LOKAL_STYLES.BottomTableRow}>
+          <SksBadge value={"6"} />
+        </View>
+        <View style={LOKAL_STYLES.BottomTableRow}>
+          <SksBadge value={"21"} />
+        </View>
+      </View>
+    </>
+  );
+}
+
+const LOKAL_STYLES = StyleSheet.create({
+  tableRow: {
+    flexDirection: "row",
+    gap: 15,
+    paddingHorizontal: 5,
+    backgroundColor: COLORS.lightGray,
+    borderBottomWidth: 2,
+    borderTopWidth: 2,
+  },
+  itemTableRow: {
+    flexDirection: "row",
+    gap: 15,
+    paddingHorizontal: 5,
+    borderBottomWidth: 2,
+  },
+  BottomTableRow: {
+    width: "25%",
+    flexDirection: "row",
+  },
+  sksContainer: {
+    alignItems: "center",
+    paddingVertical: 5,
+    width: "40%",
+    height: 45,
+    resizeMode: "contain",
+  },
+  sksBadge: {
+    backgroundColor: COLORS.gray,
+    paddingHorizontal: 4,
+    borderRadius: SIZES.radius,
+  },
+});
---
+++ src/pages/transkrip/index.js
@@ -0,0 +1,156 @@
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { COLORS, SIZES, STYLES } from "../../styles";
+import IMAGES from "../../assets/images";
+import Text from "../../components/Text";
+import TableContainer from "../../components/TableContainer";
+
+export default function TranskripScreen({ navigation }) {
+  return (
+    <View style={LOKAL_STYLES.container}>
+      <View style={LOKAL_STYLES.header}>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate("Report")}
+        >
+          <Image
+            source={IMAGES.logoKembali}
+            style={{ width: 35, height: 35 }}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+        <Text
+          color={COLORS.white}
+          bold
+          fontsize={SIZES.LargeText}
+          style={{ marginLeft: 40 }}
+        >
+          Transkrip
+        </Text>
+      </View>
+
+      <ImageBackground source={IMAGES.bgDefault} style={[STYLES.container]}>
+        <View style={LOKAL_STYLES.content}>
+          <View style={LOKAL_STYLES.infoContainerSks}>
+            <Text color={COLORS.white} bold fontsize={SIZES.mediumText}>
+              Total Sks
+            </Text>
+            <View style={LOKAL_STYLES.sksContainer}>
+              <View style={LOKAL_STYLES.sksBadge}>
+                <Text
+                  bold
+                  color={COLORS.primary}
+                  fontsize={SIZES.smallText}
+                >
+                  120
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={LOKAL_STYLES.infoContainerKualitas}>
+            <Text color={COLORS.white} bold fontsize={SIZES.mediumText}>
+              Total Angka Kualitas
+            </Text>
+            <View style={LOKAL_STYLES.sksContainer}>
+              <View style={LOKAL_STYLES.sksBadge}>
+                <Text
+                  bold
+                  color={COLORS.primary}
+                  fontsize={SIZES.smallText}
+                >
+                  54.0
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+
+        <View style={LOKAL_STYLES.infoContainerIpk}>
+            <Text color={COLORS.white} bold fontsize={SIZES.mediumText}>
+            Indeks Prestasi Kumulatif
+            </Text>
+            <View style={LOKAL_STYLES.sksContainer}>
+              <View style={LOKAL_STYLES.sksBadge}>
+                <Text
+                  bold
+                  color={COLORS.primary}
+                  fontsize={SIZES.smallText}
+                >
+                  36.2
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <TableContainer title={"MATAKULIAH WAJIB"} displayOption={"transkrip"} transkrip={"wajib"}/>
+          <TableContainer title={"MATAKULIAH PILIHAN"} displayOption={"transkrip"} transkrip={"pilihan"}/>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const LOKAL_STYLES = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  content:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 15
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: SIZES.padding2,
+    backgroundColor: COLORS.primary,
+    width: SIZES.full,
+    height: "12%",
+    paddingTop: 30,
+  },
+  infoContainerSks: {
+    width: "37%",
+    paddingLeft: SIZES.padding,
+    backgroundColor: COLORS.primary,
+    flexDirection: "row",
+    borderRadius: 5,
+    alignItems: "center",
+    marginRight: 10,
+  },
+  infoContainerKualitas: {
+    width: "62%",
+    paddingLeft: SIZES.padding,
+    backgroundColor: COLORS.primary,
+    flexDirection: "row",
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  infoContainerIpk: {
+    width: "66%",
+    paddingLeft: SIZES.padding,
+    backgroundColor: COLORS.primary,
+    flexDirection: "row",
+    borderRadius: 5,
+    alignItems: "center",
+    // alignSelf: "flex-start",
+  },
+  sksContainer: {
+    alignItems: "flex-start",
+    paddingVertical: 5,
+    width: 35,
+    height: 45,
+    resizeMode: "contain",
+    marginLeft: 14,
+  },
+  sksBadge: {
+    backgroundColor: COLORS.white,
+    paddingHorizontal: 4,
+    borderRadius: SIZES.radius,
+  },
+});
