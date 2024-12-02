193fd74bc118d803467b2fe1e51396bfd2f291f8
Meme <melisa.wijaya@ti.ukdw.ac.id>
2024-12-03 00:42:23 +0700

    Feat: Integrate Announcement List on Admin

--- src/pages/mainAdmin/listAnnouncement/index.js
+++ src/pages/mainAdmin/listAnnouncement/index.js
@@ -1,8 +1,51 @@
 import { Image, ImageBackground, ScrollView, View } from "react-native";
 import ItemAnnouncement from "../../../components/ItemAnnouncement";
 import { SIZES } from "../../../styles";
+import { FlatList } from "react-native-gesture-handler";
+import { useEffect, useState } from "react";
+import adapter from "../../../services/adapter";
 
-export default function ListAnnouncementScreen({ openDetailAnnouncement }) {
+export default function ListAnnouncementScreen({ openDetailAnnouncement, setSelectedData }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataPengumuman = await adapter.getDataPengumuman();
+      const reverseData = dataPengumuman.reverse();
+      setData(reverseData);
+      console.log(dataPengumuman);
+    };
+    fetchData();
+  }, []);
+
+  const formatDate = (tglMasuk, tglSelesai) => {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "Mei",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Okt",
+      "Nov",
+      "Des",
+    ];
+    const tglMasukArr = tglMasuk.split("-");
+    const tglSelesaiArr = tglSelesai.split("-");
+    if (tglMasukArr[1] === tglSelesaiArr[1]) {
+      return `${tglMasukArr[2]} - ${tglSelesaiArr[2]} ${
+        months[parseInt(tglMasukArr[1]) - 1]
+      } ${tglMasukArr[0]}`;
+    } else {
+      return `${tglMasukArr[2]} ${months[parseInt(tglMasukArr[1]) - 1]} - ${
+        tglSelesaiArr[2]
+      } ${months[parseInt(tglSelesaiArr[1]) - 1]} ${tglMasukArr[0]}`;
+    }
+  };
+
   return (
     <View
       style={{
@@ -11,54 +54,24 @@ export default function ListAnnouncementScreen({ openDetailAnnouncement }) {
         paddingHorizontal: 20,
       }}
     >
-      <ScrollView
-        style={{ marginBottom: 50 }}
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <ItemAnnouncement
+            announcementTitle={item.judul}
+            announcementDate={formatDate(item.tgl_masuk, item.tgl_selesai)}
+            announcementContent={item.isi}
+            announcementFileName={item.nama_lampiran}
+            onPress={() => {
+              openDetailAnnouncement();
+              setSelectedData(item);
+            }}
+          />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{ paddingBottom: 30 }}
         showsVerticalScrollIndicator={false}
-      >
-        <ItemAnnouncement
-          announcementTitle={"Informasi Registrasi"}
-          announcementDate={"03 Mar - 04 Apr"}
-          announcementContent={
-            "Teman-teman silakan untuk melakukan registrasi Sem. Gasal 2024/2025. Registrasi kelas ini online, jadi hari i"
-          }
-          anouncementFileName={"Jadwal Registrasi"}
-          onPress={openDetailAnnouncement}
-        />
-        <ItemAnnouncement
-          announcementTitle={"Informasi Registrasi"}
-          announcementDate={"03 Mar - 04 Apr"}
-          announcementContent={
-            "Teman-teman silakan untuk melakukan registrasi Sem. Gasal 2024/2025. Registrasi kelas ini online, jadi hari i"
-          }
-          anouncementFileName={"Jadwal Registrasi"}
-          onPress={openDetailAnnouncement}
-        />
-        <ItemAnnouncement
-          announcementTitle={"Informasi Registrasi"}
-          announcementDate={"03 Mar - 04 Apr"}
-          announcementContent={
-            "Teman-teman silakan untuk melakukan registrasi Sem. Gasal 2024/2025. Registrasi kelas ini online, jadi hari i"
-          }
-          anouncementFileName={"Jadwal Registrasi"}
-        />
-        <ItemAnnouncement
-          announcementTitle={"Informasi Registrasi"}
-          announcementDate={"03 Mar - 04 Apr"}
-          announcementContent={
-            "Teman-teman silakan untuk melakukan registrasi Sem. Gasal 2024/2025. Registrasi kelas ini online, jadi hari i"
-          }
-          anouncementFileName={"Jadwal Registrasi"}
-        />
-        <ItemAnnouncement
-          lastList={true}
-          announcementTitle={"Informasi Registrasi"}
-          announcementDate={"03 Mar - 04 Apr"}
-          announcementContent={
-            "Teman-teman silakan untuk melakukan registrasi Sem. Gasal 2024/2025. Registrasi kelas ini online, jadi hari i"
-          }
-          anouncementFileName={"Jadwal Registrasi"}
-        />
-      </ScrollView>
+      />
     </View>
   );
 }
