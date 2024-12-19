456cbb90dbbcb28d41636d0bb19b6c3d538e052c
Meme <melisa.wijaya@ti.ukdw.ac.id>
2024-12-20 01:55:50 +0700

    Fix: Logic for Filter Pengumuman and add refreshControl in Announcement Mahasiswa

--- src/pages/announcement/index.js
+++ src/pages/announcement/index.js
@@ -1,29 +1,38 @@
-import { FlatList, ImageBackground } from "react-native";
+import { ImageBackground, RefreshControl, ScrollView } from "react-native";
 import ItemAnnouncement from "../../components/ItemAnnouncement";
 import IMAGES from "../../assets/images";
 import SecondAppBar from "../../components/SecondAppBar";
 import { useEffect, useState } from "react";
 import DetailAnnouncement from "../../components/DetailAnnouncement";
 import adapter from "../../services/adapter";
+import { FlatList } from "react-native-gesture-handler";
+import { COLORS, SIZES } from "../../styles";
 
 export default function AnnouncementScreen({ navigation }) {
   const [showModal, setShowModal] = useState(false);
   const [data, setData] = useState([]);
   const [selectedData, setSelectedData] = useState({});
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const fetchData = async () => {
+    try {
+      setIsRefreshing(true);
       const dataPengumuman = await adapter.getDataPengumuman();
       const dateNow = new Date();
       const filteredData = dataPengumuman.filter((item) => {
-        const dateMasuk = new Date(item.tgl_masuk);
         const dateSelesai = new Date(item.tgl_selesai);
-        return dateNow >= dateMasuk && dateNow <= dateSelesai;
+        return dateNow <= dateSelesai;
       });
       const reverseData = filteredData.reverse();
       setData(reverseData);
-      console.log(dataPengumuman);
-    };
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsRefreshing(false);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
 
@@ -61,35 +70,51 @@ export default function AnnouncementScreen({ navigation }) {
   return (
     <ImageBackground source={IMAGES.bgDefault} style={{ flex: 1 }}>
       <SecondAppBar label={"Pengumuman"} navigation={navigation} />
-      <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <ItemAnnouncement
-            announcementTitle={item.judul}
-            announcementDate={formatDate(item.tgl_masuk, item.tgl_selesai)}
-            announcementContent={item.isi}
-            announcementFileName={item.nama_lampiran}
-            announcementFileType={item.nama_lampiran.split('.').pop()}
-            onPress={() => {
-              setShowModal(true);
-              setSelectedData(item);
-            }}
+      <ScrollView
+        style={{
+          flex: 1,
+          width: SIZES.full,
+          paddingHorizontal: 20,
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefreshing}
+            onRefresh={fetchData}
+            colors={[COLORS.primary]}
           />
-        )}
-        keyExtractor={(item) => item.id.toString()}
-        style={{ flex: 1, paddingHorizontal: 20, marginTop: 15 }}
-        contentContainerStyle={{ paddingBottom: 30 }}
-        showsVerticalScrollIndicator={false}
-      />
-      <DetailAnnouncement
-        showModal={showModal}
-        onClosed={onClosed}
-        title={selectedData.judul}
-        date={selectedData.tgl_masuk}
-        content={selectedData.isi}
-        imageFile={`https://mahasiswa.itda.ac.id/assets/uploads/berita/${selectedData.nama_lampiran}`}
-        fileName={selectedData.nama_lampiran}
-      />
+        }
+      >
+        <FlatList
+          data={data}
+          onScrollToTop={() => console.log("Scrolled to top")}
+          renderItem={({ item }) => (
+            <ItemAnnouncement
+              announcementTitle={item.judul}
+              announcementDate={formatDate(item.tgl_masuk, item.tgl_selesai)}
+              announcementContent={item.isi}
+              announcementFileName={item.nama_lampiran}
+              announcementFileType={item.nama_lampiran.split(".").pop()}
+              onPress={() => {
+                setShowModal(true);
+                setSelectedData(item);
+              }}
+            />
+          )}
+          keyExtractor={(item) => item.id.toString()}
+          style={{ flex: 1, marginTop: 15 }}
+          contentContainerStyle={{ paddingBottom: 30 }}
+          showsVerticalScrollIndicator={false}
+        />
+        <DetailAnnouncement
+          showModal={showModal}
+          onClosed={onClosed}
+          title={selectedData.judul}
+          date={selectedData.tgl_masuk}
+          content={selectedData.isi}
+          imageFile={`https://mahasiswa.itda.ac.id/assets/uploads/berita/${selectedData.nama_lampiran}`}
+          fileName={selectedData.nama_lampiran}
+        />
+      </ScrollView>
     </ImageBackground>
   );
 }
